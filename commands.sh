9dfcae73f6d1824a222d910a26a6fad7fbb94a17
Mason Sansom <u1329982@utah.edu>
2024-11-03 22:26:16 +0000

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -3,3 +3,6 @@ npx expo install expo-auth-session
 npm install @react-navigation/native
 npm install @react-navigation/stack
 npx expo install react-native-screens react-native-safe-area-context
+
+# how to start the project
+npx expo start -c # starts project -c clears cache, press a once its started for android
