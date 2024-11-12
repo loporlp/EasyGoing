445e7022b989a4f4893df237ca8aa818608fee91
jasl08 <jasonl49639@gmail.com>
2024-11-12 00:52:09 -0700

    Changed title background color and text color

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,12 +4,31 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index"
-                    options={{title: "Home"}}/>
+                    options={{
+                      title: "Home",
+                      headerStyle: {
+                        backgroundColor: '#24a6ad',
+                      },
+                      headerTintColor: "white",
+                      headerTitleStyle: {
+                        fontWeight: "bold",
+                      },
+                    }}/>
       <Stack.Screen name="Account" />
       <Stack.Screen name="HomeScreen" />
       <Stack.Screen name="SignInScreen" />
       <Stack.Screen name="CreateNewTrip" 
-                    options={{title: "Create New Trip"}}/>
+                    options={{
+                      title: "Create New Trip",
+                      headerStyle: {
+                        backgroundColor: '#24a6ad',
+                      },
+                      headerTintColor: "white",
+                      headerTitleStyle: {
+                        fontWeight: "bold",
+                      },
+                    }} 
+                     />
     </Stack>
   );
 }
