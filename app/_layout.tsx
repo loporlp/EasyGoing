576d671a548c8b736e41dd4bb01705429820cccb
jasl08 <jasonl49639@gmail.com>
2024-11-12 00:17:17 -0700

    Changed the titles for the screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,11 +3,13 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen name="index"
+                    options={{title: "Home"}}/>
       <Stack.Screen name="Account" />
       <Stack.Screen name="HomeScreen" />
       <Stack.Screen name="SignInScreen" />
-      <Stack.Screen name="CreateNewTrip" />
+      <Stack.Screen name="CreateNewTrip" 
+                    options={{title: "Create New Trip"}}/>
     </Stack>
   );
 }
