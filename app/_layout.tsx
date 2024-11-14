c686543ee672cc6f8705836b6390df5ca8625ecf
jasl08 <jasonl49639@gmail.com>
2024-11-13 17:30:54 -0700

    Added flag icons to the screen title for Add/Edit Destinations screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,47 +1,91 @@
 import { Stack } from "expo-router";
+import Flag from "react-native-flags";
+import { View, Text, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index"
-        options={{
-          title: "Home",
-          headerStyle: {
-            backgroundColor: '#24a6ad',
-          },
-          headerTintColor: "white",
-          headerTitleStyle: {
-            fontWeight: "bold",
-          },
-        }} />
-      <Stack.Screen name="Account" />
-      <Stack.Screen name="HomeScreen" />
-      <Stack.Screen name="SignInScreen" />
-      <Stack.Screen name="CreateNewTrip"
-        options={{
-          title: "Create New Trip",
-          headerStyle: {
-            backgroundColor: '#24a6ad',
-          },
-          headerTintColor: "white",
-          headerTitleStyle: {
-            fontWeight: "bold",
-          },
-        }}
-      />
-      <Stack.Screen name="AddEditDestinations"
-        options={{
-          // The title should be the city the user wants to go to
-          title: "Tokyo, Japan",
-          headerStyle: {
-            backgroundColor: '#24a6ad',
-          },
-          headerTintColor: "white",
-          headerTitleStyle: {
-            fontWeight: "bold",
-          },
-        }}
-      />
-    </Stack>
-  );
-}
+    <NavigationContainer>
+      <Stack>
+
+        {/*Login screen*/}
+        <Stack.Screen
+          name="index"
+          options={{
+            title: "Home",
+            headerStyle: {
+              backgroundColor: '#24a6ad',
+            },
+            headerTintColor: "white",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        />
+        {/*Account screen*/}
+        <Stack.Screen name="Account" />
+
+        {/*Home screen*/}
+        <Stack.Screen name="HomeScreen" />
+
+        {/*Sign In screen*/}
+        <Stack.Screen name="SignInScreen" />
+
+        {/*Create New Trip screen*/}
+        <Stack.Screen
+          name="CreateNewTrip"
+          options={{
+            title: "Create New Trip",
+            headerStyle: {
+              backgroundColor: '#24a6ad',
+            },
+            headerTintColor: "white",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        />
+
+        {/*Add / Edit Destinations screen*/}
+        <Stack.Screen
+          name="AddEditDestinations"
+          options={{
+            headerTitle: () => (
+              <View style={styles.titleContainer}>
+                {/* City and Country Name */}
+                <Text style={styles.headerText}>Tokyo, Japan</Text>
+                {/* Country Flag */}
+                <Flag code="JP" style={styles.flag} />
+              </View>
+            ),
+            headerStyle: {
+              backgroundColor: '#24a6ad',
+            },
+            headerTintColor: "white",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        />
+      </Stack>
+    </NavigationContainer>
+  )
+};
+
+const styles = StyleSheet.create({
+  // ==== ADD/EDIT DESTINATIONS SCREEN TITLE ==== //
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+    marginRight: 8,
+  },
+  flag: {
+    width: 22,
+    height: 22,
+  },
+})
