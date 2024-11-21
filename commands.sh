577397b5103dcc2e077a71dd13c76caf4db879ec
Mason Sansom <33166896+loporlp@users.noreply.github.com>
2024-11-21 15:03:04 -0700

    unified package dependency versions and added instructions for making sure local environment is setup properly with remote

--- commands.sh
+++ commands.sh
@@ -18,4 +18,24 @@ npm install polyline
 # how to start the project
 npx expo start -c # starts project -c clears cache, press a once its started for android
 
-npm install -g npm-check-updates
+#if you have missing dependecies run
+npm install
+
+# if you have some kind of error like "unable to resolve" you may have a dependecy mismatch, run this to correct
+
+#UNIX COMMANDS
+rm -rf node_modules package-lock.json
+npm install
+# For Windows just delete the node_modules folder and package-lock.json by hand then run
+npm install
+
+
+
+
+#Doing this will change package.json and if you update all our dependecies everyon
+# will have to run npm install to keep up
+npm install -g npm-check-updates #install update checker
+ncu #check for updates
+ncu -u #update dependecies 
+npm install #install the updates
+
