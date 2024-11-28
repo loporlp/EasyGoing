87620888a608af79abdc3d4945fc6086a7365842
Mason Sansom <33166896+loporlp@users.noreply.github.com>
2024-11-28 13:10:09 -0800

    setup server to proxy our api requests, additionally made it so api requests must have an authorization header with firebase user ID which can be found in the scripts folder. Only authenticated users will be able to send API requests

--- app/HomeScreen.tsx
+++ app/HomeScreen.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { View, Button, StyleSheet, TouchableOpacity, Text } from 'react-native';
 import { signOut } from 'firebase/auth';
 import { auth } from '../firebaseConfig';
+import {getIdToken} from '../scripts/getFirebaseID'
 
 const HomeScreen = () => {
   const handleSignOut = () => {
@@ -22,7 +23,7 @@ const HomeScreen = () => {
       <View style={styles.buttonContainer}>
         <Button title="Button 1" onPress={fetchData} />
         <View style={{ height: 20 }} />
-        <Button title="Button 2" onPress={() => {}} />
+        <Button title="Button 2" onPress={callProtectedApi} />
       </View>
     </View>
   );
@@ -31,13 +32,42 @@ const HomeScreen = () => {
 const fetchData = async () => {
   try {
     // We should put this ip into a global constant
-    const response = await fetch('http://3.145.147.136:3000/api/serverstatus');
+    const response = await fetch('http://ezgoing.app/api/serverstatus');
     const data = await response.json();
     console.log(data.message); // This should log "Hello from the server!"
   } catch (error) {
     console.error('Error fetching data:', error);
   }
 };
+
+const callProtectedApi = async () => {
+  try {
+    // Retrieve the ID token
+    const idToken = await getIdToken(auth);
+
+    const searchTerm = "McDona"
+    // Define the API endpoint
+    const apiUrl = `http://ezgoing.app/api/autocomplete?input=${searchTerm}`; // Search term is the user inputted that we are auto completeing
+
+    // Make the API call
+    const response = await fetch(apiUrl, {
+      method: "GET", // Or "POST", "PUT", etc.
+      headers: {
+        Authorization: `Bearer ${idToken}`, // Include the ID token in the header
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log("API Response:", data); // Handle the response
+  } catch (error) {
+    console.error("Error calling API:", error);
+  }
+};
+
 
 const styles = StyleSheet.create({
   container: {
