25ae048dca5941833bd9738017a283bb8d7083fd
Mason Sansom <masonsansom02@gmail.com>
2024-12-04 16:25:52 -0700

    quick changes to main after merge

--- scripts/fetchData.js
+++ scripts/fetchData.js
@@ -3,11 +3,11 @@ export const fetchData = async () => {
   const timeout = setTimeout(() => controller.abort(), 5000); // Set timeout to 5 seconds
 
   try {
-    //const response = await fetch('http://ezgoing.app/api/serverstatus', {
-      //signal: controller.signal, // Attach the abort signal
-    //});
-    //const data = await response.json();
-    //console.log(data.message); // This should log "Server is Running"
+    const response = await fetch('http://ezgoing.app/api/serverstatus', {
+      signal: controller.signal, // Attach the abort signal
+    });
+    const data = await response.json();
+    console.log(data.message); // This should log "Server is Running"
     return true;
 
   } catch (error) {
