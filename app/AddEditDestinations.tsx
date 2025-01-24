51f7384b1aca655d9c48d61d12de803d7d78ef95
jasl08 <jasonl49639@gmail.com>
2025-01-23 18:34:33 -0700

    Set up SwipeListView and swipeable actions for each added destination

--- app/AddEditDestinations.tsx
+++ app/AddEditDestinations.tsx
@@ -7,6 +7,8 @@ import { storeData, getData } from '../scripts/localStore.js';
 import DynamicImage from '../components/DynamicImage';
 import { useNavigation } from "@react-navigation/native";
 import { Ionicons } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { SwipeListView } from 'react-native-swipe-list-view';
 
 //stores destination object in local storage with key 'destination'
 async function storeDestination(key: string, destination: any) {
@@ -160,10 +162,55 @@ const AddEditDestinations = () => {
     const [priority, setPriority] = useState("");
     const [typedNotes, setNotes] = useState("");
 
+    // Swipable List components
+    const [swipeStatus, setSwipeStatus] = useState<{ [key: string]: boolean }>({});
+
+    // Function to handle swipe state
+    const handleSwipeChange = (swipeData: any) => {
+        const { key, value } = swipeData;
+        if (value !== 0) {
+            // If swiping, remove border radius
+            setSwipeStatus((prevState) => ({ ...prevState, [key]: true }));
+        } else {
+            // If swipe is reset, restore border radius
+            setSwipeStatus((prevState) => ({ ...prevState, [key]: false }));
+        }
+    };
+
+    const renderHiddenItem = ({ item, rowMap }: any) => (
+        <View style={[styles.hiddenItem, { height: 100 }]}>
+            <TouchableOpacity onPress={() => { }} style={[styles.editButton, { width: Math.abs(rightOpenValue / 2) }]}>
+                <Ionicons name="pencil-sharp" size={25} color={"white"} />
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => { }} style={[styles.deleteButton, { width: Math.abs(rightOpenValue / 2) }]}>
+                <Ionicons name="trash-bin" size={25} color={"white"} />
+            </TouchableOpacity>
+        </View>
+    );
+
+    const renderItem = ({ item }: any) => {
+        const isSwiped = swipeStatus[item.key];
+        return (
+            <View
+                style={[
+                    styles.destination,
+                    { borderRadius: isSwiped ? 0 : 10 },
+                ]}
+            >
+                <View style={{flex: 1, flexDirection: "row", justifyContent: "flex-start", alignItems: "center"}}>
+                    <DynamicImage placeName={item.name} containerStyle={styles.destinationImage} imageStyle={styles.destinationImage} />
+                    <Text style={{flex: 1}}>{item.name}</Text>
+                </View>
+            </View>
+        );
+    }
+
+    const rightOpenValue = -150;
+
     return (
         <View style={styles.container}>
-
-            {/* Background image + TextInputs */}
+            {/* Background image */}
             <DynamicImage placeName="New York City" containerStyle={styles.backgroundImage} imageStyle={styles.backgroundImage} />
             <View style={styles.darkOverlay}></View>
 
@@ -172,21 +219,61 @@ const AddEditDestinations = () => {
                     <Ionicons name="arrow-back-outline" size={30} color={"white"} />
                 </TouchableOpacity>
 
-                {/* Group of text fields for travelers and budget */}
-                <View style={{marginTop: 15}}>
-                    <TextInput placeholder="New York City, USA" placeholderTextColor="black" style={styles.input} />
-                    <TextInput placeholder="Fri, Jul. 11 - Tue, Jul. 15" placeholderTextColor="black" style={styles.input} />
+                {/* Group of text fields */}
+                <View style={{ marginTop: 15 }}>
+                    <TouchableOpacity style={[styles.input, { flex: 1, flexDirection: "row", alignItems: 'center' }]}>
+                        <Ionicons name="location" size={22} color={"#24a6ad"} />
+                        <TextInput placeholder="New York City, USA" placeholderTextColor="black" style={{ fontSize: 16, marginLeft: 5 }} />
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={[styles.input, { flex: 1, flexDirection: "row", alignItems: 'center' }]}>
+                        <Ionicons name="calendar-sharp" size={22} color={"#24a6ad"} />
+                        <TextInput placeholder="Fri, Jul. 11 - Tue, Jul. 15" placeholderTextColor="black" style={{ fontSize: 16, marginLeft: 5 }} />
+                    </TouchableOpacity>
+
                     <View style={styles.travelersAndBudgetTextField}>
-                        <TextInput placeholder="1 traveler" placeholderTextColor="black" keyboardType="numeric" style={styles.travelerInput} />
-                        <TextInput placeholder="$1,700" placeholderTextColor="black" keyboardType="numeric" style={styles.budgetInput} />
+                        <TouchableOpacity style={[styles.travelerInput, { flex: 1, flexDirection: "row", alignItems: 'center' }]}>
+                            <Ionicons name="people" size={22} color={"#24a6ad"} />
+                            <TextInput placeholder="1 traveler" placeholderTextColor="black" keyboardType="numeric" style={{ fontSize: 16, marginLeft: 5 }} />
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={[styles.budgetInput, { flex: 1, flexDirection: "row", alignItems: 'center' }]}>
+                            <Ionicons name="wallet" size={22} color={"#24a6ad"} />
+                            <TextInput placeholder="$1,700" placeholderTextColor="black" keyboardType="numeric" style={{ fontSize: 16, marginLeft: 5 }} />
+                        </TouchableOpacity>
                     </View>
                 </View>
+            </View>
 
-                <View style={styles.destinationScreen}>
+            {/* Scrollable window that displays all the destinations added */}
+            <View style={styles.destinationScreen}>
+                <Text style={{ fontSize: 22, fontWeight: "700", marginBottom: 10, marginHorizontal: 20, marginTop: 10 }}>Destinations Selected <Text style={{color: "#24a6ad"}}>({destinations.length})</Text>:</Text>
+                <SwipeListView
+                    data={destinations.map((item, index) => ({ ...item, key: `${index}` }))}
+                    renderItem={renderItem}
+                    renderHiddenItem={renderHiddenItem}
+                    leftOpenValue={rightOpenValue}
+                    rightOpenValue={rightOpenValue}
+                    friction={60}
+                    tension={30}
+                    contentContainerStyle={{ paddingHorizontal: 20 }}
+                    onSwipeValueChange={handleSwipeChange}>
+                </SwipeListView>
+            </View>
 
-                </View>
+            <View style={styles.navBar}>
+                <TouchableOpacity style={{ padding: 10, marginLeft: 20 }}>
+                    <Ionicons name="add" size={30} color={"#24a6ad"} />
+                </TouchableOpacity>
+                <TouchableOpacity style={{ padding: 10 }}>
+                    <MaterialCommunityIcons name="application-import" size={30} color={"#24a6ad"} />
+                </TouchableOpacity>
+                <TouchableOpacity style={{ padding: 10, marginRight: 20 }}>
+                    <Ionicons name="arrow-forward-circle-sharp" size={30} color={"#24a6ad"} />
+                </TouchableOpacity>
             </View>
         </View>
+
     );
 }
 
@@ -222,9 +309,8 @@ const styles = StyleSheet.create({
         borderColor: '#999',
         borderBottomWidth: 1,
         marginRight: 10,
-        fontSize: 16,
         backgroundColor: "white",
-        paddingLeft: 20,
+        paddingLeft: 10,
         borderRadius: 10,
         paddingTop: 5,
         paddingBottom: 5,
@@ -241,7 +327,7 @@ const styles = StyleSheet.create({
         borderBottomWidth: 1,
         fontSize: 16,
         backgroundColor: "white",
-        paddingLeft: 20,
+        paddingLeft: 10,
         borderRadius: 10,
         paddingTop: 5,
         paddingBottom: 5,
@@ -260,16 +346,17 @@ const styles = StyleSheet.create({
 
     input: {
         height: 40,
-        width: "100%",
+        maxWidth: "100%",
+        overflow: "hidden",
         borderColor: '#999',
         borderBottomWidth: 1,
         marginBottom: 10,
-        fontSize: 16,
         backgroundColor: "white",
         borderRadius: 10,
-        paddingLeft: 20,
+        paddingLeft: 10,
         paddingTop: 5,
         paddingBottom: 5,
+        paddingRight: 25,
         shadowColor: "#333333",
         shadowOffset: { width: 1, height: 2 },
         shadowOpacity: 0.1,
@@ -279,11 +366,72 @@ const styles = StyleSheet.create({
     // ==== BOTTOM SCREEN ==== //
     destinationScreen: {
         backgroundColor: "#F4F4F4",
-        height: "100%",
-        marginHorizontal: -20,
-        marginTop: 20,
+        flex: 1,
         borderTopLeftRadius: 10,
-        borderTopRightRadius: 10
+        borderTopRightRadius: 10,
+        marginTop: -40,
+    },
+
+    navBar: {
+        flexDirection: "row",
+        backgroundColor: "white",
+        justifyContent: "space-between",
+        alignItems: "center",
+        height: "8%",
+        shadowColor: "#333333",
+        shadowOffset: { width: 1, height: 2 },
+        shadowOpacity: 0.3,
+        shadowRadius: 3,
+    },
+
+    destination: {
+        backgroundColor: "white",
+        width: "100%",
+        height: 100,
+        marginBottom: 10,
+        borderRadius: 10,
+        shadowColor: "#333333",
+        shadowOffset: { width: 1, height: 2 },
+        shadowOpacity: 0.3,
+        shadowRadius: 3,
+    },
+
+    destinationImage: {
+        width: 100,
+        height: 100,
+        resizeMode: "cover",
+        overflow: "hidden",
+        borderRadius: 10,
+        marginLeft: -30
+    },
+
+    hiddenItem: {
+        backgroundColor: "white",
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "flex-end",
+        alignItems: 'center',
+        borderRadius: 10,
+        width: "100%",
+        marginBottom: 10,
+    },
+
+    deleteButton: {
+        backgroundColor: "red",
+        height: "100%",
+        paddingHorizontal: 15,
+        justifyContent: "center",
+        alignItems: "center",
+        borderTopRightRadius: 10,
+        borderBottomRightRadius: 10
+    },
+
+    editButton: {
+        backgroundColor: "#24a6ad",
+        height: "100%",
+        paddingHorizontal: 15,
+        justifyContent: "center",
+        alignItems: "center",
     }
 });
 
